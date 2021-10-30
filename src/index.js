f6cf36865cad01edc91f7fbbdbce6f108a4809bd
PawelRadawiec <pawel.radaw@gmail.com>
2021-10-30 16:43:56 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+require('./models/Article');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.json());
+
+const mongodbUri = 'mongodb://127.0.0.1:27017/reactnative-course';
+mongoose.connect(mongodbUri);
+
+mongoose.connection.on('connected', () => {
+  console.log('Connect to mongodb');
+});
+
+mongoose.connection.on('error', (error) => {
+  console.error(error);
+});
+
+app.get('/hello', (req, res) => {
+  res.send('Hello there!');
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
---
+++ src/models/Article.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+  author: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+    rating: {
+      positive: Number,
+      negative: Number,
+    },
+  },
+});
+
+const articleSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  comments: [commentSchema],
+});
